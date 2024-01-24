172b4f39e7718a7a90f3cab290610111e5db7df9
PolliBu <8polli.bu@gmail.com>
2024-01-24 21:48:56 +0200

    Add useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import { useState } from 'react'
+import { useState } from 'react';
 import { ContactForm } from './components/ContactForm/ContactForm';
 import { SearchBox } from './components/SearchBox/SearchBox';
 import { ContactList } from './components/ContactList/ContactList';
@@ -6,12 +6,12 @@ import './App.css';
 
 export function App() {
   // const [count, setCount] = useState(0)
-  const contacts = [
+  const [contacts, setcontacts] = useState([
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
+  ]);
 
   return (
     <>
