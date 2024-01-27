10661db3f4e5c5b7c585dda2ba1ff14f4062e8d9
PolliBu <8polli.bu@gmail.com>
2024-01-27 18:21:08 +0200

    Add task 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ContactForm } from './components/ContactForm/ContactForm';
 import { SearchBox } from './components/SearchBox/SearchBox';
 import { ContactList } from './components/ContactList/ContactList';
@@ -6,12 +6,14 @@ import './App.css';
 
 export function App() {
   const [nameFilter, setNameFilter] = useState('');
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(
+    JSON.parse(window.localStorage.getItem('contacts')) || [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+  );
 
   const visibleUsers = contacts.filter(contact =>
     contact.name.toLowerCase().includes(nameFilter.toLowerCase()),
@@ -27,6 +29,10 @@ export function App() {
       return [...prevContacts, newContact];
     });
   };
+
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
     <>
