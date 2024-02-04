d4f16d8280b0534700df2244439764c12adb0071
PolliBu <8polli.bu@gmail.com>
2024-02-04 13:55:46 +0200

    Add initialState

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import { useState, useEffect } from 'react';
 import { ContactForm } from './components/ContactForm/ContactForm';
 import { SearchBox } from './components/SearchBox/SearchBox';
 import { ContactList } from './components/ContactList/ContactList';
-import './App.css';
+
+const initialState = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
 export function App() {
   const [nameFilter, setNameFilter] = useState('');
   const [contacts, setContacts] = useState(
-    JSON.parse(window.localStorage.getItem('contacts')) || [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    JSON.parse(window.localStorage.getItem('contacts')) || initialState,
   );
 
   useEffect(() => {
